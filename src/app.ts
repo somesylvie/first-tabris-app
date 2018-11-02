f585d1418579815013bf4bb70eaa8e062c596865
Sylvie <sschuresko@madisoncollege.edu>
2018-11-02 12:32:52 -0500

    Added the rest of the taco info and shake

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import {ui, Button, TextView, NavigationView, ScrollView, Page} from 'tabris';
+import {ActivityIndicator, ui, Button, TextView, NavigationView, ScrollView, Page} from 'tabris';
 import {getRandomTaco, TacoResponse} from './connectors/taco';
 
 let navigationView = new NavigationView({
@@ -6,7 +6,7 @@ let navigationView = new NavigationView({
 }).appendTo(ui.contentView);
 
 let page = new Page({
-  title: 'Learnin\' some tabris.js'
+  title: 'Taco Shaker'
 }).appendTo(navigationView);
 
 let scrollView = new ScrollView({
@@ -16,13 +16,79 @@ let scrollView = new ScrollView({
 new TextView({
   top: 0, left: 16, right: 16,
   id: 'title',
-  text: 'For future title',
+  text: 'Loading...',
   font: 'bold 28px'
 }).appendTo(scrollView);
 
-getRandomTaco().then((tacoResponse) => {
-  console.log(tacoResponse);
-});
+new TextView({
+  top: ['prev()', 16], left: 16, right: 16,
+  text: 'Base Layer',
+  font: 'thin italic 26px sans-serif'
+}).appendTo(scrollView);
+
+new TextView({
+  top: ['prev()', 0], left: 32, right: 16,
+  markupEnabled: true,
+  id: 'base-layer'
+}).appendTo(scrollView);
+new TextView({
+  top: ['prev()', 0], left: 16, right: 16,
+  text: 'Mixin',
+  font: 'thin italic 26px sans-serif'
+}).appendTo(scrollView);
+
+new TextView({
+  top: ['prev()', 0], left: 32, right: 16,
+  markupEnabled: true,
+  id: 'mixin'
+}).appendTo(scrollView);
+
+new TextView({
+  top: ['prev()', 0], left: 16, right: 16,
+  text: 'Condiment',
+  font: 'thin italic 26px sans-serif'
+}).appendTo(scrollView);
+
+new TextView({
+  top: ['prev()', 0], left: 32, right: 16,
+  markupEnabled: true,
+  id: 'condiment'
+}).appendTo(scrollView);
+
+new TextView({
+  top: ['prev()', 0], left: 16, right: 16,
+  text: 'Recipe',
+  font: 'thin italic 26px sans-serif'
+}).appendTo(scrollView);
+
+new TextView({
+  top: ['prev()', 0], left: 32, right: 16
+  , markupEnabled: true
+  , id: 'recipe'
+}).appendTo(scrollView);
+
+function getTaco() {
+  let activityIndicator = new ActivityIndicator({centerX: 0, centerY: 0}).appendTo(ui.contentView);
+  getRandomTaco().then((tacoResponse) => {
+    console.log(tacoResponse);
+    scrollView.apply({
+      '#title': { text: tacoResponse.title}
+      , '#recipe': { text: tacoResponse.recipe}
+      , '#condiment': { text: tacoResponse.condiment}
+      , '#mixin': { text: tacoResponse.mixin}
+      , '#base-layer': { text: tacoResponse.baseLayer}
+    })
+  }).catch((error) => {
+      // noinspection TsLint
+      console.error(error);
+  }).then(() => {
+      activityIndicator.dispose();
+  });
+}
+
+getTaco();
+
+shake.startWatch(getTaco, 40, console.error);
 
 // let button = new Button({
 //   centerX: 0, top: 100,
